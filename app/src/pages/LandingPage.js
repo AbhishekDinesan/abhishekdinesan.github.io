545c7654ba5bc54a396512785ec864f2b669572d
Abhishek Dinesan <abhidines04@gmail.com>
2025-04-13 21:31:37 -0700

    another cleanup

--- app/src/pages/LandingPage.js
+++ app/src/pages/LandingPage.js
@@ -1,11 +1,10 @@
-import { Card, HStack } from "@chakra-ui/react";
-import {CustomCard, FaceCard} from "../components/Card"
+import {HStack } from "@chakra-ui/react";
+import {FaceCard} from "../components/Card"
 import {CardTypes} from '../utils/enums'
 
 const LandingPage = () => {
     return(
         <div>
-            {/* <CustomCard cardTitle='yo' buttonText="Click Me" cardDescription="Description 1" /> */}
             <HStack>
             <FaceCard cardType={CardTypes.ace} />
             <FaceCard cardType={CardTypes.two} />
