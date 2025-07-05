93ec53875dfe53f15c5a00bcdc79b523c688d468
Abhishek Dinesan <abhidines04@gmail.com>
2025-07-05 09:22:01 -0400

    gallery layout

--- app/src/pages/GalleryPage.js
+++ app/src/pages/GalleryPage.js
@@ -2,10 +2,14 @@
 
 
 
-import { Alert } from "@chakra-ui/react"
+import { Alert, Box, SimpleGrid} from "@chakra-ui/react"
 
 const GalleryPage = () => {
+
+  const photos = [] //figure out cloud storage mechanism and iterate through  
+
     return (
+      <Box>
         <Alert.Root status="error">
         <Alert.Indicator />
         <Alert.Content>
@@ -15,6 +19,16 @@ const GalleryPage = () => {
           </Alert.Description>
         </Alert.Content>
       </Alert.Root>
+        <SimpleGrid columns={6} gap="40px" height={"100vh"} padding="20px">
+          <Box bg="gray.100" gridColumn="span 3" gridRow="span 3" />
+          <Box bg="gray.200" gridColumn="span 2" gridRow="span 2" />
+          <Box bg="gray.300" gridColumn="span 1" gridRow="span 2" />
+          <Box bg="gray.400" gridColumn="span 3" gridRow="span 1" />
+          <Box bg="gray.500" gridColumn="span 2" gridRow="span 2" />
+          <Box bg="gray.600" gridColumn="span 3" gridRow="span 2" />
+          <Box bg="gray.700" gridColumn="span 1.5." gridRow="span 2" />
+      </SimpleGrid>
+      </Box>
     )
 }
 
