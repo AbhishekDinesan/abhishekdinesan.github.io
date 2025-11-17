271890b2344df5e86bcbd6c6ff4af8280aa2ed3c
Abhishek Dinesan <abhidines04@gmail.com>
2025-11-16 23:07:40 -0800

    small update

--- app/src/pages/CareerPage.js
+++ app/src/pages/CareerPage.js
@@ -1,4 +1,3 @@
-import SkillsDashboard from '../components/SkillsDashboard'
 import CareerTimeline from '../components/CareerTimeline'
 import DualDegreeSection from '../components/DualDegreeSection'
 import ProjectShowcase from '../components/ProjectShowcase'
@@ -6,7 +5,6 @@ import ProjectShowcase from '../components/ProjectShowcase'
 const CareerPage = () => {
     return (
         <div style={{ padding: 'clamp(0.5rem, 2vw, 1rem)' }}>
-            <SkillsDashboard />
             <CareerTimeline />
             <DualDegreeSection />
             <ProjectShowcase />
